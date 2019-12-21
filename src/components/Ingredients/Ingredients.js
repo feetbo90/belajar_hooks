d92f073ca68fcdd6ba5b3abc7fde93c1c394b12a
mhdiqbalpradipta <mhdiqbalpradipta@gmail.com>
2019-12-21 17:33:07 +0700

    halo

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+
+  const [userIngredients, setUserIngredients] = useState([]);
+  const [statusSimpan, setStatusSimpan] = useState(false);
+
+  useEffect(() => {
+    fetch("https://react-hook-project.firebaseio.com/ingredients.json")
+    .then(response => response.json())
+    .then(responseData => {
+      const loadedIngredients = []
+      for(const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount
+        })
+      }
+      setUserIngredients(loadedIngredients)
+    })
+  }, [])
+
+  const addUserIngredientsHandler = ingredient => {
+
+  }
+
+  const simpanStatus = (data) => {
+    setStatusSimpan(true)
+    setUserIngredients([...userIngredients, data])
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm simpanStatus={simpanStatus} onAddIngredients={addUserIngredientsHandler}/>
 
       <section>
         <Search />
         {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} onRemoveItem={() =>{}}/>
       </section>
     </div>
   );
